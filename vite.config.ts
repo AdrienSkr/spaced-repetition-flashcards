7b4d2092b7848dd27b157ed490d93037abd89e6c
Adrien <85847277+AdrienSkr@users.noreply.github.com>
2026-04-25 18:42:37 -0400

    fix: update base path to match new repo name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vitest/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/pairWise-Cards/',
+  base: '/spaced-repetition-flashcards/',
   publicDir: 'public',
   plugins: [preact()],
   build: {
